63d8f43b1d78e5a55d0bbf48fe0119ccc362c0ea
Mayank-MP05 <mayank5pande@gmail.com>
2020-09-24 20:07:30 +0530

    Sample files

--- file.test.py
+++ file.test.py
@@ -1,4 +1,18 @@
 import classBased as cb
-Huffboth = cb.Huffboth
+Huffboth = cb.Huffboth()
 
-print(Huffboth)
+file = "sample-file.txt"
+
+with open(file, 'rb') as r:
+    content = r.read()
+    encoded_text = Huffboth.Encode(str(content))
+    
+    decoded_text = Huffboth.Decode(encoded_text)
+    if(str(content) == decoded_text):
+        print("File Compression and Decompression Sucessful!")
+    else:
+        print("Failed!")
+        
+    fileOP = open("compressed_file.bin", "wb")
+    fileOP.write(bytes(encoded_text, 'utf-8'))
+    fileOP.close()
